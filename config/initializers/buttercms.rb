d37e8bc7a6ca0e4ae4cc970fd725d3ca447679fc
zachkrasner <zachkrasner@gmail.com>
2016-09-06 15:28:28 -0700

    Added ButterCMS blog

---
+++ config/initializers/buttercms.rb
@@ -0,0 +1,6 @@
+require 'buttercms-ruby'
+
+# If you added the Heroku Butter add-on, ENV["BUTTER_TOKEN"] will be defined.
+# Otherwise, grab your token at https://buttercms.com/profile/ and either set it below
+# or in an environment variable.
+ButterCMS::api_token = "cce69c0fa190e9a93c47a61dee36522f1e6b4baf"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  root 'buttercms/posts#index', :defaults=> {:page => 1}
+
+  scope :module => 'buttercms' do
+    get '/categories/:slug' => 'categories#show', :as => :buttercms_category
+    get '/author/:slug' => 'authors#show', :as => :buttercms_author
+
+    get '/blog/rss' => 'feeds#rss', :format => 'rss', :as => :buttercms_blog_rss
+    get '/blog/atom' => 'feeds#atom', :format => 'atom', :as => :buttercms_blog_atom
+    get '/blog/sitemap.xml' => 'feeds#sitemap', :format => 'xml', :as => :buttercms_blog_sitemap
+
+    get '/blog(/page/:page)' => 'posts#index', :defaults => {:page => 1}, :as => :buttercms_blog
+    get '/blog/:slug' => 'posts#show', :as => :buttercms_post
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
